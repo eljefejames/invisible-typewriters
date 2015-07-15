1e63ce5e1df4e24b7f1cbe80d098908407d13ecb
eljefejames <lukejamesmail@gmail.com>
2015-07-15 15:33:57 +0700

    randoms

---
+++ alt-tutorial/src/learningjs.js
@@ -0,0 +1,14 @@
+
+
+var isEven = function(number) {
+  if ((number % 2)<1) {
+      return true;
+      }
+      else {
+          return false;
+      }
+
+};
+
+isEven(5);
+console.log(isEven(5));
